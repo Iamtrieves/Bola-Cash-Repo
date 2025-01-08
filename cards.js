4119c1ee85fc1ccfe2bcab426466f7f85e6547b7
Gabriel Indok David <indokdavid2020@gmail.com>
2025-01-08 16:12:27 +0100

    Added Images

--- cards.js
+++ cards.js
@@ -1,21 +1,25 @@
+import RecycleBottleCanImage from "./src/assets/Images/landing-page-images/recycle-bottle-can.jpg";
+import MedalImage from "./src/assets/Images/landing-page-images/medal-image.jpg";
+import RecyclingTipsImage from "./src/assets/Images/landing-page-images/group-recycle-image.png";
+
 const cards = [
   {
     id: "1",
-    image: "./src/assets/Images/landing-page-images/recycle-bottle-can.jpg",
+    image: RecycleBottleCanImage,
     title: "Collection of Recyclables",
     description:
       "Make a difference with every item recycled! Our collection of recyclables feature makes it easy to dispose of materials responsibly. Join us in keeping our planet clean and green. Start recycling today!",
   },
   {
     id: "2",
-    image: "./src/assets/Images/landing-page-images/medal-image.jpg",
+    image: MedalImage,
     title: "Rewards",
     description:
       "Earn rewards while you save the planet! Our rewards program allowes you to earn points for every item recycled. Start collecting rewards and make a positive impact on the environment today!",
   },
   {
     id: "3",
-    image: "./src/assets/Images/landing-page-images/group-recycle-image.png",
+    image: RecyclingTipsImage,
     title: "Recycling Tips/Education",
     description:
       "Eco-wisdom at your fingertips! Explore our recycling tips and education section for practical adivice on reducing waste and recycling smarter. Join us in creating a greener tomorrow!",
