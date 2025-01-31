7dda08e6cf59dfa0518266742e33b2919bedc262
Gabriel Indok David <indokdavid2020@gmail.com>
2025-01-31 12:58:59 +0100

    Modified Routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { AuthProvider } from "./Components/auth";
 import { PopUpProvider } from "./Components/PopUpContext";
 import { LoadingProvider } from "./assets/context/LoadingAnimationContext";
 import { RouterProvider } from "react-router-dom";
-import router from "./routes";
+import router from "./Routes";
 
 const App = () => {
   return (
